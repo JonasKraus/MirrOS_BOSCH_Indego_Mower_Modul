2e8794e98f7a4ce3b4065c76a7d0c6788ebd9598
JonasKraus <jonaskraus@gmx.de>
2018-07-26 19:31:53 +0200

    fixed map style in css

--- frontend/template.php
+++ frontend/template.php
@@ -1,36 +1,36 @@
 <h2 class="module__title">Indego Mower</h2>
+<div>
+    <div id="indego_mower_map"></div>
+    <table class="indego_mower_table">
+        <tbody>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('state') ?></td>
+            <td id="indego_mower_state">-</td>
+        </tr>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('mowed') ?></td>
+            <td id="indego_mower_mowed">-</td>
+        </tr>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('mow mode') ?></td>
+            <td id="indego_mower_mowmode">-</td>
+        </tr>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('runtime total') ?></td>
+            <td id="indego_mower_runtimeTotal">-</td>
+        </tr>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('runtime session') ?></td>
+            <td id="indego_mower_runtimeSession">-</td>
+        </tr>
+        <tr>
+            <td class="indego_mower_text_align_right"><?php echo _('map update avail') ?></td>
+            <td id="indego_mower_mapUpdate">-</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 
-<table class="indego_mower_table" width="100%">
-    <tbody>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('state') ?></td>
-        <td id="indego_mower_state">-</td>
-    </tr>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('mowed') ?></td>
-        <td id="indego_mower_mowed">-</td>
-    </tr>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('mow mode') ?></td>
-        <td id="indego_mower_mowmode">-</td>
-    </tr>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('runtime total') ?></td>
-        <td id="indego_mower_runtimeTotal">-</td>
-    </tr>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('runtime session') ?></td>
-        <td id="indego_mower_runtimeSession">-</td>
-    </tr>
-    <tr>
-        <td class="indego_mower_text_align_right"><?php echo _('map update avail') ?></td>
-        <td id="indego_mower_mapUpdate">-</td>
-    </tr>
-    </tbody>
-
-</table>
-
-<div id="indego_mower_map"></div>
 
 
 
